605624a0a84e6f305deaedcc890a4468e13e98b2
Yamiro22 <moyamingo1234@gmail.com>
2023-11-19 22:24:18 +0300

    firstcommit

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# catsay_tui
+
+
+```markdown
+# catsay_tai
+
+catsay_tai is a Rust project that allows you to create ASCII art of a cat saying a message.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+To use catsay_tai, add the following to your `Cargo.toml` file:
+
+```toml
+[dependencies]
+catsay_tai = "0.1.0"
+```
+
+## Usage
+
+Add the following to your Rust code:
+
+```rust
+use catsay_tai::Catsay;
+
+fn main() {
+    let cat = Catsay::new("Hello, Rust!");
+    println!("{}", cat);
+}
+```
+
+## Examples
+
+Here are some examples of using catsay_tai:
+
+```rust
+use catsay_tai::Catsay;
+
+fn main() {
+    let cat = Catsay::new("Meow!");
+    println!("{}", cat);
+}
+```
+
+This will output:
+
+```
+  /\_/\ 
+ ( o.o ) 
+ > ^ <
+Meow!
+```
+
+Feel free to modify and experiment with the message to create your own cat sayings!
+
+## Contributing
+
+If you'd like to contribute to catsay_tai, please follow the [contribution guidelines](CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+```
+
+
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use cursive::event::Key;
+use cursive::traits::Nameable;
+use cursive::views::{Checkbox, Dialog, EditView, ListView, TextView};
+use cursive::Cursive;
+use std::mem::forget;
+
+// Wrap all form fields value in one struct
+// so we can pass them around easily
+struct CatsayOptions<'a> {
+    message: &'a str,
+    dead: bool,
+}
+
+fn input_step(siv: &mut Cursive) {
+    siv.add_layer(
+        Dialog::new()
+            .title("Please fill out the form for the cat")
+            .content(
+                ListView::new()
+                    .child("Message:", EditView::new().with_id("message"))
+                    .child("Dead?", Checkbox::new().with_id("dead")),
+            )
+            .button("OK", |s| {
+                let message = s
+                    .call_on_name("message", |t: &mut EditView| t.get_content())
+                    .unwrap();
+                let is_dead = s
+                    .call_on_name("dead", |t: &mut Checkbox| t.is_checked())
+                    .unwrap();
+                let options = CatsayOptions {
+                    message: &message,
+                    dead: is_dead,
+                };
+                result_step(s, &options) // [2]
+            }),
+    );
+}
+
+fn result_step(siv: &mut Cursive, options: &CatsayOptions) {
+    let eye = if options.dead { "x" } else { "o" };
+
+    let cat_text = format!(
+        "{msg}
+\\
+  \\
+    /\\_/\\
+   ( {eye} {eye} )
+   =( I )=",
+        msg = options.message,
+        eye = eye
+    );
+    siv.pop_layer(); // [3]
+    siv.add_layer(
+        // [4]
+        Dialog::text(cat_text)
+            .title("The cat says...")
+            .button("OK", |s| s.quit()),
+    );
+}
+
+fn main() {
+    let mut siv = cursive::default();
+    input_step(&mut siv);
+    siv.run();
+}
